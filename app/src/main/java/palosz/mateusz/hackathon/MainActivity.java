ef1b109e5e23963041f0213c24ad1af40510e052
Walkingbeer <gleadr123@gmail.com>
2018-10-10 13:07:15 +0200

    End button

--- app/src/main/java/palosz/mateusz/hackathon/MainActivity.java
+++ app/src/main/java/palosz/mateusz/hackathon/MainActivity.java
@@ -3,13 +3,13 @@ package palosz.mateusz.hackathon;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button1, button2, button3, button4;
     Typeface custom_fonts;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
         button2.setTypeface(custom_fonts);
         button3.setTypeface(custom_fonts);
         button4.setTypeface(custom_fonts);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                System.exit(0);
+            }
+        });
+
     }
 }
