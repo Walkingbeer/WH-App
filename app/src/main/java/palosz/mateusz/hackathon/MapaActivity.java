89c6fd48b11a7c752854d94bf6c33f4484e25ef5
Mateusz <gleadr123@gmail.com>
2018-10-13 15:20:05 +0200

    4

--- app/src/main/java/palosz/mateusz/hackathon/MapaActivity.java
+++ app/src/main/java/palosz/mateusz/hackathon/MapaActivity.java
@@ -13,7 +13,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -42,11 +45,19 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected boolean gps_enabled, network_enabled;
     Location locationGPS;
 
+    public Toolbar custom_toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        custom_toolbar = findViewById(R.id.toolbar);
+        //getSupportActionBar().hide();
+
+        setSupportActionBar(custom_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         getSupportActionBar().hide();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -100,6 +111,54 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onResume();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+
+
+        //Toast toast = Toast.makeText(context, "Action"+id, duration);
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast toast = Toast.makeText(context, "Action 1", duration);
+            toast.show();
+            return true;
+        }
+        if (id == R.id.action2) {
+            Toast toast = Toast.makeText(context, "Action 2", duration);
+            toast.show();
+            return true;
+        }
+        if (id == R.id.action3) {
+            Toast toast = Toast.makeText(context, "Action 3", duration);
+            toast.show();
+            return true;
+        }
+        if (id == R.id.action4) {
+            Toast toast = Toast.makeText(context, "Action 4", duration);
+            toast.show();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
